aa5f4f8e4afe2e9a408d00c5f6dfbd8f3321a9ab
fayyoz24 <fayyozusmon@gmail.com>
2025-05-22 16:05:39 +0500

    xaxa

--- beauty/barber/urls.py
+++ beauty/barber/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('work-posts/', WorkPostListCreateView.as_view(), name='workpost-list-create'),
 
     path('barbers/create/', CreateBarberProfileView.as_view(), name='create-barber-profile'),
-
+    path('barbers/<int:pk>/', BarberDetailView.as_view(), name='barber-detail'),
     path('all-work-posts/', AllWorkPostListCreateView.as_view(), name='all-posts'),
     path('all-work-posts/<int:pk>/', WorkPostDetailView.as_view(), name='all-posts/int'),
     path('appointments/', AppointmentListCreateView.as_view(), name='appointment-list-create'),
